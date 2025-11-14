694953f9cdabf0aeeca4533b24b5e63780f9423c
Santiago Botero <157855016+LePeanutButter@users.noreply.github.com>
2025-11-14 09:07:38 -0500

    feat(server): add /game proxy middleware

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import devtoolsJson from 'vite-plugin-devtools-json';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv, type UserConfigExport, type ConfigEnv } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit(), devtoolsJson()]
-});
+export default ({ mode }: ConfigEnv) => {
+  const env = loadEnv(mode, process.cwd(), 'VITE_');
+
+  return defineConfig({
+    plugins: [sveltekit(), devtoolsJson()],
+    server: {
+      proxy: {
+        '/game': {
+          target: env.VITE_AZURE_GAME_URL,
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/game/, '/')
+        }
+      }
+    }
+  }) as UserConfigExport;
+};
