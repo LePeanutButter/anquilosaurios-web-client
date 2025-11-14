40a5ff372c45ff9076fcefa0ba2847cc4308a7f9
Santiago Botero <157855016+LePeanutButter@users.noreply.github.com>
2025-11-14 09:13:23 -0500

    chore(format): add prettier scripts for formatting and checking

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,18 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig, loadEnv, type UserConfigExport, type ConfigEnv } from 'vite';
 
 export default ({ mode }: ConfigEnv) => {
-  const env = loadEnv(mode, process.cwd(), 'VITE_');
+	const env = loadEnv(mode, process.cwd(), 'VITE_');
 
-  return defineConfig({
-    plugins: [sveltekit(), devtoolsJson()],
-    server: {
-      proxy: {
-        '/game': {
-          target: env.VITE_AZURE_GAME_URL,
-          changeOrigin: true,
-          rewrite: path => path.replace(/^\/game/, '/')
-        }
-      }
-    }
-  }) as UserConfigExport;
+	return defineConfig({
+		plugins: [sveltekit(), devtoolsJson()],
+		server: {
+			proxy: {
+				'/game': {
+					target: env.VITE_AZURE_GAME_URL,
+					changeOrigin: true,
+					rewrite: (path) => path.replace(/^\/game/, '/')
+				}
+			}
+		}
+	}) as UserConfigExport;
 };
